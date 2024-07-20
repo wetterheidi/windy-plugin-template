550d32efbd6b260583a56ddb513ded687353ad2f
“wetterheidi“ <43071660+wetterheidi@users.noreply.github.com>
2024-07-20 14:52:26 +0200

    Name geändert

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,15 +2,15 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-pressure-difference',
-    version: '0.0.2',
+    version: '0.0.3',
     title: 'Pressure Difference Charts Alps',
     icon: '⛰️',
     description: 'Displays pressure difference diagrams for various cross sections.',
     author: 'HS',
-    repository: 'https://github.com/wetterheidi/windy-plugin-pressure-difference-charts',
+    repository: 'https://github.com/wetterheidi/windy-plugin-pressure-difference.git',
     desktopUI: 'rhpane',
     mobileUI: 'fullscreen',
-    routerPath: '/foehn-chart-alps',
+    routerPath: '/pressure-difference',
     desktopWidth: 800,
     private: true,
 };
